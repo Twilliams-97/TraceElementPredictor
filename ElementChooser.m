0e89a54e1393134cc015c721e6dad34ab22447c6
Twilliams-97 <73836410+Twilliams-97@users.noreply.github.com>
2021-12-28 14:13:55 +0100

    Update ElementChooser.m
    Replaced for loop and function with a find function to get index of elements to be deleted, then remove values at these indices

--- ElementChooser.m
+++ ElementChooser.m
@@ -1,22 +1,8 @@
 %% Removes elements that aren't wanted
 
-for elementnumber = length(IncludedElements):-1:1
-     
-    if ~IncludedElements(elementnumber)
-        
-        [combinedfullelements,SparseElements,ElementNames,NormaliseElements] = elementchoice(elementnumber,combinedfullelements,SparseElements,ElementNames,NormaliseElements);
+IndexofElementstoRemove = find(IncludedElements == 0);
 
-    end
-
-end
-
-%%%%%%%% Function
-
-function [combinedfullelements,SparseElements,ElementNames,NormaliseElements] = elementchoice(elementnumber,combinedfullelements,SparseElements,ElementNames,NormaliseElements)
-
-    combinedfullelements(elementnumber,:) = [];
-    SparseElements(elementnumber,:) = [];
-    ElementNames(elementnumber,:) = [];
-    NormaliseElements(elementnumber,:) = [];
-
-end
+combinedfullelements(IndexofElementstoRemove,:) = [];
+SparseElements(IndexofElementstoRemove,:) = [];
+ElementNames(IndexofElementstoRemove,:) = [];
+NormaliseElements(IndexofElementstoRemove,:) = [];
