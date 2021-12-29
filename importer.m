eea237b04a41e8d360e96c502537b17adbb31433
Twilliams-97 <73836410+Twilliams-97@users.noreply.github.com>
2021-12-29 14:32:41 +0100

    Update importer.m
    Set the testing arrays to use the same function as the others
    
    Made excel table formats consistent

--- importer.m
+++ importer.m
@@ -10,20 +10,17 @@ ElementNames = readtable(ExcelFile, 'Sheet', 'Names', 'VariableNamingRule', 'pre
 ElementNames = ElementNames{:,1};
 AllElementNames = ElementNames;
 
-%% Import CI Chondrite values to normalise plots! ATM these are PUM
+%% Import CI Chondrite values to normalise plots!
 
 Normalise = readtable(ExcelFile, 'Sheet', 'CI Chondrite', 'VariableNamingRule', 'preserve');
-
-NormaliseElements = Normalise{2:end, 2};
-
+NormaliseElements = Normalise{1:end, 2};
 AllNormaliseElements = NormaliseElements;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%% Actual Sparse Data
 
 if ImportBasalt   
     
-    MareBasaltElements = elementreader(ExcelFile,'All_MareNoSm');
-    MareBasaltElements = MareBasaltElements(2:end,:);
+    MareBasaltElements = elementreader(ExcelFile,'All_Mare');
 
 end
 
@@ -67,7 +64,7 @@ end
 
 if ImportNealBasalt
     NealBasaltElements = elementreader(ExcelFile,'NealBasalt'); %Basalt
-    NealBasaltElements = NealBasaltElements(2:end,:);
+    
     %RemoveApollo11Basalts
     NealBasaltElements(:,1:8) = [];
 end
@@ -81,43 +78,25 @@ end
 if ImportLaPazMareBasaltMeteorite
     LaPazMareBasaltMeteoriteElements = elementreader(ExcelFile,'LaPazMareBasaltMeteorite'); %Mare Basalt Meteorite
 end
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%% Data Used For Testing
 
-%% Import Full Data from Excel (all elements characterised,modern)
+if ImportTestFull % Import Full Data from Excel (all elements characterised,modern)
 
-if ImportTestFull
-
-    TestFull = readtable(ExcelFile, 'Sheet', 'TestFull', 'VariableNamingRule', 'preserve');
-
-    TestFull = TestFull{:, :};  % Get relevant elements
+    TestFull = elementreader(ExcelFile,'TestFull');
 
 end
 
+if ImportTestSparse % Import Sparse Data from Excel (elements missing, legacy)
 
-
-%% Import Sparse Data from Excel (elements missing, legacy)
-
-if ImportTestSparse
-
-    TestSparse = readtable(ExcelFile, 'Sheet', 'TestSparse', 'VariableNamingRule', 'preserve');
-
-    % Get relevant elements
-
-    TestSparse = TestSparse{:, :};
-    TestSparse(isnan(TestSparse)) = 9999;  %Sets all NaN values to 9999. Needed for computational methods.
+    TestSparse = elementreader(ExcelFile,'TestSparse');
 
 end
-
-
 
 function elementread = elementreader(ExcelFile,SheetName)
-
-    
-    elementread = readtable(ExcelFile,'Sheet', SheetName, 'VariableNamingRule', 'preserve');
-            
-    elementread = elementread{:, 2:end};
-            
+   
+    elementread = readtable(ExcelFile,'Sheet', SheetName, 'VariableNamingRule', 'preserve');           
+    elementread = elementread{:, 2:end};      
     elementread(isnan(elementread)) = 9999;
             
-
 end
